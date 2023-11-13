c084f518562a4805a52251a55616642c1c489e7a
Shoghik <shoghik.kachatryan@dci-student.org>
2023-11-13 23:04:36 +0100

    first commit

--- utils/src/main/java/com/epam/utils/StringUtils.java
+++ utils/src/main/java/com/epam/utils/StringUtils.java
@@ -2,6 +2,15 @@ package com.epam.utils;
 
 public class StringUtils {
     public static boolean isPositiveNumber(String str) {
-         //here magic will happen
+        if (str == null || str.isBlank()) {
+            return false; // Empty or null string is not a positive number
+        }
+
+        try {
+            double number = Double.parseDouble(str);
+            return number > 0;
+        } catch (NumberFormatException e) {
+            return false; // Unable to parse as a number
+        } //here magic will happen
     }
 }
