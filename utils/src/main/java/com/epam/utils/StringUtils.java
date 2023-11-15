13587e52f59a87171ad3887f6bdb50b65526db01
Shoghik <shoghik.kachatryan@dci-student.org>
2023-11-15 23:15:55 +0100

    change StringUtils function

--- utils/src/main/java/com/epam/utils/StringUtils.java
+++ utils/src/main/java/com/epam/utils/StringUtils.java
@@ -2,12 +2,15 @@ package com.epam.utils;
 
 public class StringUtils {
     public static boolean isPositiveNumber(String str) {
+        if (str == null) {
+            return false;
+        }
+
         if (str.startsWith("+")) {
             return isPositiveNumber(str.substring(1));
         }
 
-        if (str == null || str.startsWith("-") ||
-                (str.startsWith("0") && !str.startsWith("0."))) {
+        if (str.startsWith("-") || (str.startsWith("0") && !str.startsWith("0."))) {
             return false;
         }
         try {
